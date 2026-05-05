bb7e137691355d3520a01ef0df4d67fa2c3bc554
Dmytro Sukharev <dmytro.sukharev@suponoholdings.com>
2026-05-06 01:50:53 +0300

    Interaction Update
    New interaction system, no xr interactables
    Climbable ladders
    Fixed grabbed object jittering
    Better Ui interaction
    Layer cleanup

--- Assets/Scripts/Systems/Player/PlayerGrab.cs
+++ Assets/Scripts/Systems/Player/PlayerGrab.cs
@@ -1,61 +1,95 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using UnityEngine.InputSystem;
 
 namespace Systems.Player
 {
     public class PlayerGrab : MonoBehaviour
     {
-        [SerializeField] private XRBaseInteractor _interactor;
+        [SerializeField] private InputActionReference _grabAction;
+        [SerializeField] private Rigidbody _interactableBody;
+        [SerializeField] private string _grabbedLayerName;
+        [SerializeField] private string _releasedLayerName;
+        
         private FixedJoint _joint;
+        private int _grabbedLayer;
+        private int _releasedLayer;
 
-        private void HandleSelectEntered(SelectEnterEventArgs args)
+        private void OnGrab(InputAction.CallbackContext context)
         {
-            var interactable = args.interactableObject;
-            if (interactable == null)
+            if (_interactableBody == null)
             {
                 return;
             }
-            var body = interactable.transform.GetComponent<Rigidbody>();
+            _joint = gameObject.AddComponent<FixedJoint>();
+            _joint.connectedBody = _interactableBody;
+            _interactableBody.gameObject.layer = _grabbedLayer;
+        }
+
+        private void OnRelease(InputAction.CallbackContext context)
+        {
+            if (_joint)
+            {
+                _joint.connectedBody.gameObject.layer = _releasedLayer;
+                Destroy(_joint);
+            }
+        }
+
+        private void SubscribeGrabAction()
+        {
+            if (!_grabAction)
+            {
+                Debug.LogWarning("PlayerGrab Grab Action missing");
+                return;
+            }
+            
+            _grabAction.action.performed += OnGrab;
+            _grabAction.action.canceled += OnRelease;
+        }
+
+        private void UnsubscribeGrabAction()
+        {
+            if (!_grabAction)
+            {
+                return;
+            }
+            _grabAction.action.performed -= OnGrab;
+            _grabAction.action.canceled -= OnRelease;
+        }
+        
+        private void InitGrabbableLayers()
+        {
+            _grabbedLayer =  LayerMask.NameToLayer(_grabbedLayerName);
+            _releasedLayer =  LayerMask.NameToLayer(_releasedLayerName);
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            var body = other.attachedRigidbody;
             if (!body)
             {
                 return;
             }
-
-            _joint = gameObject.AddComponent<FixedJoint>();
-            _joint.autoConfigureConnectedAnchor = false;
-            _joint.connectedBody = body;
-            _joint.connectedAnchor = transform.InverseTransformPoint(interactable.transform.position);
-            Debug.Log($"Grabbing {interactable.transform.gameObject.name}");
+            _interactableBody = body;
         }
 
-        private void HandleSelectExited(SelectExitEventArgs args)
+        private void OnTriggerExit(Collider other)
         {
-            var interactable = args.interactableObject;
-            if (_joint)
+            var body = other.attachedRigidbody;
+            if (_interactableBody == body)
             {
-                Destroy(_joint);
+                _interactableBody = null;
             }
-            Debug.Log($"Releasing {interactable.transform.gameObject.name}");
         }
-        
+
         private void Awake()
         {
-            if (!_interactor)
-            {
-                Debug.LogWarning("PlayerGrab Interactor missing");
-                return;
-            }
-            
-            _interactor.selectEntered.AddListener(HandleSelectEntered);
-            _interactor.selectExited.AddListener(HandleSelectExited);
+            SubscribeGrabAction();
+            InitGrabbableLayers();
         }
         
         private void OnDestroy()
         {
-            _interactor.selectEntered.RemoveListener(HandleSelectEntered);
-            _interactor.selectExited.RemoveListener(HandleSelectExited);
+            UnsubscribeGrabAction();
         }
     }
 }
